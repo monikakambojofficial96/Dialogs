0c2bbbca2a81b7d98929b42a13a06139937ae1bc
monikakambojofficial96 <monikakambojofficial96@gmail.com>
2019-03-04 14:55:50 +0530

    Show Dialog Box

---
+++ app/src/main/java/com/example/dialogs/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dialogs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button =(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               openDialog();
+
+            }
+        });
+
+
+    }
+
+    public void openDialog(){
+
+         ExampleDialog exampleDialog=new ExampleDialog();
+
+         exampleDialog.show(getSupportFragmentManager(),"example dialog");
+
+    }
+
+}
